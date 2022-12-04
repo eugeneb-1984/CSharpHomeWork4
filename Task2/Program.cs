d196b0856025b5bd7412bcfc2f9580cf3a194cc1
Eugene Bogdanov <eugeneb1984@gmail.com>
2022-12-04 12:31:28 +0300

    Added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+//Принимаем число
+int GetNumber(string message) {
+    bool isNumber = false;
+    int Number = 0;
+    while(!isNumber) {
+        Console.Write(message);
+        string input = Console.ReadLine();
+        if(Int32.TryParse(input, out Number)) {
+            isNumber = true;
+        }
+        else {
+            Console.WriteLine("Вы ошиблись при вводе. Введите число.");
+        }
+    }
+    return Number;
+}
+
+//Складываем цифры в числе
+int SumChars(int number) {
+    string NumString = number.ToString();
+    int index = 0;
+    int result = 0;
+    while (index < NumString.Length) {
+        Int32.TryParse(NumString[index].ToString(), out int increment);
+        result += increment;
+        index++;
+    }
+    return result;
+}
+
+//Вызываем методы
+int number = GetNumber("Введите число: ");
+Console.WriteLine($"Сумма цифр числа {number}: {SumChars(number)}");
